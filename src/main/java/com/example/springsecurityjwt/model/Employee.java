5ef34ee8f977a0ba380c280606379f7d2c848409
supriyo <das.supriyogp@gmail.com>
2023-03-15 12:26:36 +0530

    commit for Employee.java

--- src/main/java/com/example/springsecurityjwt/model/Employee.java
+++ src/main/java/com/example/springsecurityjwt/model/Employee.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
+/*import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,7 +15,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor*/
 @Entity
 @Table(name="employees")
 public class Employee {
@@ -30,6 +30,59 @@ public class Employee {
 	@Column(name="last_name")
 	private String lastName;
 	
+	
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public Employee( String firstName, String lastName, String email) {
+		super();
+	
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	@Column(name="email")
 	private String email;
+	
+	
+	
+	
 }
